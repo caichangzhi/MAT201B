f4900838df125eb6154ba4f836e27145f2eb40a3
Changzhi Cai <35616479+caichangzhi@users.noreply.github.com>
2020-01-29 11:58:36 -0800

    Add files via upload

--- assignment/point-geometry.glsl
+++ assignment/point-geometry.glsl
@@ -8,10 +8,16 @@ layout(triangle_strip, max_vertices = 4) out;
 uniform mat4 al_ProjectionMatrix;
 uniform float pointSize;
 
-in Vertex { vec4 color; }
+in Vertex {
+  vec4 color;
+  float size;
+}
 vertex[];
 
-out Fragment { vec4 color; }
+out Fragment {
+  vec4 color;
+  vec2 mapping;
+}
 fragment;
 
 void main() {
@@ -19,21 +25,26 @@ void main() {
   vec4 v = gl_in[0].gl_Position;  // al_ModelViewMatrix * gl_Position
 
   float r = pointSize;
+  r *= vertex[0].size;
 
   gl_Position = m * (v + vec4(-r, -r, 0.0, 0.0));
   fragment.color = vertex[0].color;
+  fragment.mapping = vec2(-1.0, -1.0);
   EmitVertex();
 
   gl_Position = m * (v + vec4(r, -r, 0.0, 0.0));
   fragment.color = vertex[0].color;
+  fragment.mapping = vec2(1.0, -1.0);
   EmitVertex();
 
   gl_Position = m * (v + vec4(-r, r, 0.0, 0.0));
   fragment.color = vertex[0].color;
+  fragment.mapping = vec2(-1.0, 1.0);
   EmitVertex();
 
   gl_Position = m * (v + vec4(r, r, 0.0, 0.0));
   fragment.color = vertex[0].color;
+  fragment.mapping = vec2(1.0, 1.0);
   EmitVertex();
 
   EndPrimitive();
